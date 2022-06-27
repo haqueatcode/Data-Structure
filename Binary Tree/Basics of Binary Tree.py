86731923ab433d604c7d6e549646ed62e85ad3a6
EatCode <enamul_cse@yahoo.com>
2022-06-27 00:43:03 -0700

    Update Basics of Binary Tree.py

--- Binary Tree/Basics of Binary Tree.py
+++ Binary Tree/Basics of Binary Tree.py
@@ -5,7 +5,29 @@ Basics of Binary Tree:
     - A tree
     - Must have at most 2 children
     - Root node, left child, and right child node
+    
+- A tree node can be root, left, or right
+
+- A tree node contains 3 types of information
+    1. data/info
+    2. pointer to the left child
+    3. pointer to the right child
+
 """
+
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.data = value
+        
+        
+if __name__ == "__main__":
+    
+    root = Node(3)
+    root.left = Node(5)
+    root.right = Node(10)
+    root.left.left = Node(15)
 
 
     
